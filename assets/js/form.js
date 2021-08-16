991b2ec18543473328b7bd80442cfc90bbde0cf7
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-08-16 20:56:51 +0700

    fix

---
+++ assets/js/form.js
@@ -0,0 +1,10 @@
+function saveLocalStorage(field)
+{
+    localStorage.setItem(field, document.getElementById(field).value);
+}
+
+function showPart(current, part)
+{
+    document.getElementById(current).hidden = true;
+    document.getElementById(part).hidden = false;
+}
