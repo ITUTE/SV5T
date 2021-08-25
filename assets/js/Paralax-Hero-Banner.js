c11aabbfde67e0711bb811008ab3c09bdfb5b4f4
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-08-26 00:39:48 +0700

    remove log

--- assets/js/Paralax-Hero-Banner.js
+++ assets/js/Paralax-Hero-Banner.js
@@ -1,13 +1,13 @@
-
 var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
+
+function parallax() {
     var scrolled = $(window).scrollTop();
     $('.jumbotron').css('background-position', 'center ' + -(scrolled * 0.2) + 'px');
-    console.log(jumboHeight-scrolled);
+    //console.log(jumboHeight-scrolled);
 }
 
-$(window).scroll(function(e){
-    
+$(window).scroll(function(e) {
+
     parallax();
-    
+
 });
